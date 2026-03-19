8a4483ee55c1c9f6d17c72726cb2860cacf7623e
nicoleroot <18917916101@qq.com>
2026-03-19 20:58:29 +0800

    Update Junit test

--- src/test/java/seedu/duke/BitbitesTest.java
+++ src/test/java/seedu/duke/BitbitesTest.java
@@ -3,18 +3,46 @@ package seedu.duke;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BitbitesTest {
+
+    private FoodList foodList;
+    private UserInterface ui;
+
+    @BeforeEach
+    void setUp() {
+        foodList = new FoodList();
+        ui = new UserInterface();
+        foodList.addFood(new Food("Burger", 450, 30, "2026-03-16"));
+        foodList.addFood(new Food("Salad", 200, 10, "2026-03-17"));
+    }
+
     @Test
     public void sampleTest() {
         assertTrue(true);
     }
 
     @Test
-    public void sampleTest4() {
-        assertTrue(true);
+    void helpCommand_doesNotExit() {
+        boolean isExit = Parser.parse("help", foodList, ui);
+        assertFalse(isExit);
     }
 
+    @Test
+    void deleteCommand_validIndex_reducesSize() {
+        int sizeBefore = foodList.size();
+        Parser.parse("delete 1", foodList, ui);
+        assertEquals(sizeBefore - 1, foodList.size());
+    }
+
+    @Test
+    void deleteCommand_doesNotExit() {
+        boolean isExit = Parser.parse("delete 1", foodList, ui);
+        assertFalse(isExit);
+    }
+    
     @Test
     public void listTest() {
         assertTrue(true);
