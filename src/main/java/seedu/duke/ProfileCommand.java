61cafff6fe2fd855f5ec947bb98c4254a801b76f
Bryan Yeo <bryaniyeo3125@gmail.com>
2026-03-27 15:27:25 +0800

    Added skeleton code for profile command for milestone 2

---
+++ src/main/java/seedu/duke/ProfileCommand.java
@@ -0,0 +1,11 @@
+package seedu.duke;
+
+// TODO: Implement Profile command (v2.0)
+public class ProfileCommand extends Command {
+
+    @Override
+    public boolean execute(FoodList foodList, UserInterface ui) {
+        // TODO: Implement profile logic
+        return false;
+    }
+}
